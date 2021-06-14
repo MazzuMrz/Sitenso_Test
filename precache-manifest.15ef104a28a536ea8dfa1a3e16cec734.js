69674f11be2bcb38822db1a6a9e4c5452392f564
MazzuMrz <maruzzamatias@gmail.com>
2021-06-14 00:29:19 -0300

    Updates

---
+++ precache-manifest.15ef104a28a536ea8dfa1a3e16cec734.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5953ca32e89008df87c8f98954e974",
+    "url": "/Sitenso_Test/index.html"
+  },
+  {
+    "revision": "977723b2421165845eb5",
+    "url": "/Sitenso_Test/static/css/2.69a2fe0f.chunk.css"
+  },
+  {
+    "revision": "4f587bdec7e1be2ea4e7",
+    "url": "/Sitenso_Test/static/css/4.a8471b74.chunk.css"
+  },
+  {
+    "revision": "28b7949c6aacd7834388",
+    "url": "/Sitenso_Test/static/css/5.d104cf8c.chunk.css"
+  },
+  {
+    "revision": "35dcabc50fdde05bd91b",
+    "url": "/Sitenso_Test/static/css/6.938c68d3.chunk.css"
+  },
+  {
+    "revision": "7d1d94ba668bcea31b63",
+    "url": "/Sitenso_Test/static/css/main.cf847e70.chunk.css"
+  },
+  {
+    "revision": "977723b2421165845eb5",
+    "url": "/Sitenso_Test/static/js/2.3c111f76.chunk.js"
+  },
+  {
+    "revision": "b841ff62dde27523c1574e3892b2561b",
+    "url": "/Sitenso_Test/static/js/2.3c111f76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90b21819873a031139bf",
+    "url": "/Sitenso_Test/static/js/3.b501f353.chunk.js"
+  },
+  {
+    "revision": "fe07165234709e61e0cdc05d4056de5c",
+    "url": "/Sitenso_Test/static/js/3.b501f353.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f587bdec7e1be2ea4e7",
+    "url": "/Sitenso_Test/static/js/4.08bc93d9.chunk.js"
+  },
+  {
+    "revision": "28b7949c6aacd7834388",
+    "url": "/Sitenso_Test/static/js/5.f559beef.chunk.js"
+  },
+  {
+    "revision": "35dcabc50fdde05bd91b",
+    "url": "/Sitenso_Test/static/js/6.b50b9449.chunk.js"
+  },
+  {
+    "revision": "02c968e8942a25ef807a",
+    "url": "/Sitenso_Test/static/js/7.b91e4dc0.chunk.js"
+  },
+  {
+    "revision": "147f3ea6f45aad8a2c98",
+    "url": "/Sitenso_Test/static/js/8.6ff2b4e1.chunk.js"
+  },
+  {
+    "revision": "7d1d94ba668bcea31b63",
+    "url": "/Sitenso_Test/static/js/main.12f08c34.chunk.js"
+  },
+  {
+    "revision": "82483ef0c5c4d7bf6928",
+    "url": "/Sitenso_Test/static/js/runtime-main.effa7725.js"
+  },
+  {
+    "revision": "b1b0e9087780461d47f996cbdf08b5e3",
+    "url": "/Sitenso_Test/static/media/favicon.b1b0e908.png"
+  },
+  {
+    "revision": "3d2fa2e544004aef2ea641698b715af6",
+    "url": "/Sitenso_Test/static/media/feather.3d2fa2e5.woff"
+  },
+  {
+    "revision": "6a750f0119ba9c8ab8a994a4796599a6",
+    "url": "/Sitenso_Test/static/media/feather.6a750f01.ttf"
+  },
+  {
+    "revision": "931c9e74d0b20947054bfe6a5b74a838",
+    "url": "/Sitenso_Test/static/media/feather.931c9e74.eot"
+  },
+  {
+    "revision": "b23df4db71a29dbb733a0e555a7db8f9",
+    "url": "/Sitenso_Test/static/media/feather.b23df4db.svg"
+  },
+  {
+    "revision": "222e25f53e8f2a25ab93bb09f2b99e07",
+    "url": "/Sitenso_Test/static/media/google.222e25f5.svg"
+  },
+  {
+    "revision": "fd58a052054c690506118c4b7a0f731d",
+    "url": "/Sitenso_Test/static/media/login.fd58a052.png"
+  },
+  {
+    "revision": "b1b0e9087780461d47f996cbdf08b5e3",
+    "url": "/Sitenso_Test/static/media/logo.b1b0e908.png"
+  },
+  {
+    "revision": "eb4e894d949eb0ce8f21368b70f3dd3b",
+    "url": "/Sitenso_Test/static/media/vuesax-login-bg.eb4e894d.jpg"
+  }
+]);
