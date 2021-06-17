b9372910fed0001a1b9a7212185bd2ff9544ed66
MazzuMrz <maruzzamatias@gmail.com>
2021-06-17 18:39:38 -0300

    - problemas + lindo :).

--- src/configs/themeConfig.js
+++ src/configs/themeConfig.js
@@ -2,16 +2,16 @@
 
 //Template config options
 const themeConfig = {
-  layout: "horizontal", // options[String]: "vertical"(default), "horizontal"
+  layout: "vertical", // options[String]: "vertical"(default), "horizontal"
   theme: "light", // options[String]: 'light'(default), 'dark', 'semi-dark'
   sidebarCollapsed: true, // options[Boolean]: true, false(default)
   navbarColor: "warning", // options[String]: default / primary / success / danger / info / warning / dark
-  navbarType: "sticky", // options[String]: floating(default) / static / sticky / hidden
+  navbarType: "floating", // options[String]: floating(default) / static / sticky / hidden
   footerType: "static", // options[String]: static(default) / sticky / hidden
   disableCustomizer: true, // options[Boolean]: true, false(default)
   hideScrollToTop: false, // options[Boolean]: true, false(default)
   menuTheme: "danger", // options[String]: primary / success / danger / info / warning / dark
-  direction: "rtl" // options[String] : ltr / rtl
+  direction: "ltr" // options[String] : ltr / rtl
 }
 
 export default themeConfig
